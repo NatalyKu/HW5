f8c4d5d376d45f38663e6878d1f91a91b697b651
NatalyKu <nataly.kuksa.uk@gmail.com>
2024-07-16 10:10:29 +0200

    Changed names

--- test.py
+++
@@ -1,5 +0,0 @@
-salary_text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
-import re
-pattern = r'\d*\.\d{2}'
-for salary in re.findall(pattern,salary_text):
-    print(salary)
